3318bb4c388a729cf2ce608f2d1053dbce7f5e59
Fernando Araújo <papucho86@gmail.com>
2013-07-25 16:49:55 +0100

    updates capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,8 @@ set :repository,  "git@github.com:groupbuddies/group-buddies-site.git"
 set :branch, "capistrano"
 set :scm, :git
 set :deploy_via, :remote_cache
+set :deploy_to, "/home/deploy/#{application}"
+set :use_sudo, false
+set :user, "deploy"
 
 server '37.139.7.70', :app, :web
-
-after 'deploy:setup', 'create_sockets_folder'
-
-desc "Creates sockets folder"
-task :create_sockets_folder do
-  run "mkdir #{shared_path}/sockets"
-end
