1fa1f8d6a0f8c63fedca7bd8c2f2afd728e48560
Miguel Palhas <mpalhas@gmail.com>
2015-01-08 12:12:31 +0000

    Why this nonsense? helpers/helpers was already being required 3 lines before

--- app.rb
+++ app.rb
@@ -9,18 +9,6 @@ Dotenv.load
 require './helpers/helpers.rb'
 require './config/initializers/load_keys.rb'
 
-# require_relative does not exist in ruby 1.8.7
-# This is a fallback -- http://stackoverflow.com/a/4718414/951432
-unless Kernel.respond_to?(:require_relative)
-  module Kernel
-    def require_relative(path)
-      require File.join(File.dirname(caller[0]), path.to_str)
-    end
-  end
-end
-
-require_relative 'helpers/helpers'
-
 configure do
   set :sass, style: :compressed
 
